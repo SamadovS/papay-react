d435af9790628ca62291f9b877448290a91305c5
Sirojiddin <siroj.origin.profile>
2023-11-18 08:31:53 +0900

    feat: homepage ads section publishing

--- src/app/screens/HomePage/bestDishes.tsx
+++ src/app/screens/HomePage/bestDishes.tsx
@@ -1,21 +1,132 @@
 import React from "react";
-import { Box, Button, Container } from "@mui/material";
-import { Stack } from "@mui/system";
-
-import Card from "@mui/joy/Card";
-import CardCover from "@mui/joy/CardCover";
-import CardContent from "@mui/joy/CardContent";
-import Typography from "@mui/joy/Typography";
-import LocationOnRoundedIcon from "@mui/icons-material/LocationOnRounded";
-import { CssVarsProvider } from "@mui/joy/styles";
-import { CardOverflow, IconButton } from "@mui/joy";
-import { Favorite } from "@mui/icons-material";
-import VisibilityIcon from "@mui/icons-material/Visibility";
+import { Box, Container, Stack } from "@mui/material";
+import MonetizationOn from "@mui/icons-material/MonetizationOn";
 
 export function BestDishes() {
     return (
-        <div className="top_restaurant_frame">
-            <Container>BestDishes</Container>
+        <div className="best_dishes_frame">
+            <Container>
+                <Stack flexDirection={"column"} alignItems={"center"}>
+                    <Box className="category_title">Trenddagi Ovqatlar</Box>
+                    <Stack sx={{ mt: "43px" }} flexDirection={"row"}>
+                        {/* Dish 1 */}
+                        <Box className="dish_box">
+                            <Stack
+                                className="dish_img"
+                                sx={{
+                                    backgroundImage:
+                                        'url("https://images.unsplash.com/photo-1602253057119-44d745d9b860?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8ZGlzaHxlbnwwfHwwfHx8MA%3D%3D")',
+                                }}
+                            >
+                                <div className={"dish_sale"}>normal size</div>
+                                <div className={"view_btn"}>
+                                    Batafsil Ko'rish
+                                    <img
+                                        src={"/icons/arrow_right.svg"}
+                                        style={{ marginLeft: "9px" }}
+                                    />
+                                </div>
+                            </Stack>
+                            <Stack className={"dish_desc"}>
+                                <span className={"dish_title_text"}>
+                                    Chicken Mayo
+                                </span>
+                                <span className={"dish_desc_text"}>
+                                    <MonetizationOn />
+                                    11
+                                </span>
+                            </Stack>
+                        </Box>
+
+                        {/* Dish 2 */}
+                        <Box className="dish_box">
+                            <Stack
+                                className="dish_img"
+                                sx={{
+                                    backgroundImage:
+                                        'url("https://images.unsplash.com/photo-1602253057119-44d745d9b860?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8ZGlzaHxlbnwwfHwwfHx8MA%3D%3D")',
+                                }}
+                            >
+                                <div className={"dish_sale"}>normal size</div>
+                                <div className={"view_btn"}>
+                                    Batafsil Ko'rish
+                                    <img
+                                        src={"/icons/arrow_right.svg"}
+                                        style={{ marginLeft: "9px" }}
+                                    />
+                                </div>
+                            </Stack>
+                            <Stack className={"dish_desc"}>
+                                <span className={"dish_title_text"}>
+                                    Chicken Mayo
+                                </span>
+                                <span className={"dish_desc_text"}>
+                                    <MonetizationOn />
+                                    11
+                                </span>
+                            </Stack>
+                        </Box>
+
+                        {/* Dish 3 */}
+                        <Box className="dish_box">
+                            <Stack
+                                className="dish_img"
+                                sx={{
+                                    backgroundImage:
+                                        'url("https://images.unsplash.com/photo-1602253057119-44d745d9b860?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8ZGlzaHxlbnwwfHwwfHx8MA%3D%3D")',
+                                }}
+                            >
+                                <div className={"dish_sale"}>normal size</div>
+                                <div className={"view_btn"}>
+                                    Batafsil Ko'rish
+                                    <img
+                                        src={"/icons/arrow_right.svg"}
+                                        style={{ marginLeft: "9px" }}
+                                    />
+                                </div>
+                            </Stack>
+                            <Stack className={"dish_desc"}>
+                                <span className={"dish_title_text"}>
+                                    Chicken Mayo
+                                </span>
+                                <span className={"dish_desc_text"}>
+                                    <MonetizationOn />
+                                    11
+                                </span>
+                            </Stack>
+                        </Box>
+
+                        {/* Dish 4 */}
+                        <Box className="dish_box">
+                            <Stack
+                                className="dish_img"
+                                sx={{
+                                    backgroundImage:
+                                        'url("https://images.unsplash.com/photo-1602253057119-44d745d9b860?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8ZGlzaHxlbnwwfHwwfHx8MA%3D%3D")',
+                                }}
+                            >
+                                <div className={"dish_sale"}>normal size</div>
+                                <div className={"view_btn"}>
+                                    Batafsil Ko'rish
+                                    <img
+                                        src={"/icons/arrow_right.svg"}
+                                        style={{ marginLeft: "9px" }}
+                                    />
+                                </div>
+                            </Stack>
+                            <Stack className={"dish_desc"}>
+                                <span className={"dish_title_text"}>
+                                    Chicken Mayo
+                                </span>
+                                <span className={"dish_desc_text"}>
+                                    <MonetizationOn />
+                                    11
+                                </span>
+                            </Stack>
+                        </Box>
+                    </Stack>
+                </Stack>
+            </Container>
         </div>
     );
 }
