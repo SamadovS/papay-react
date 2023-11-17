17feab0ebe815ad1642cbed68308d2f0c0f44ebc
Sirojiddin <siroj.origin.profile>
2023-11-17 17:49:16 +0900

    fix: modify homepage css styles

--- src/app/screens/HomePage/bestDishes.tsx
+++ src/app/screens/HomePage/bestDishes.tsx
@@ -1,5 +1,16 @@
 import React from "react";
-import { Container } from "@mui/material";
+import { Box, Button, Container } from "@mui/material";
+import { Stack } from "@mui/system";
+
+import Card from "@mui/joy/Card";
+import CardCover from "@mui/joy/CardCover";
+import CardContent from "@mui/joy/CardContent";
+import Typography from "@mui/joy/Typography";
+import LocationOnRoundedIcon from "@mui/icons-material/LocationOnRounded";
+import { CssVarsProvider } from "@mui/joy/styles";
+import { CardOverflow, IconButton } from "@mui/joy";
+import { Favorite } from "@mui/icons-material";
+import VisibilityIcon from "@mui/icons-material/Visibility";
 
 export function BestDishes() {
     return (
