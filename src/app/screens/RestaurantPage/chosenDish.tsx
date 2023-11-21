163488c9637196f8a62428f5e1121f3a0cc64000
Sirojiddin <siroj.origin.profile>
2023-11-21 15:06:50 +0900

    feat: restaurant screen develop chosenDish component

--- src/app/screens/RestaurantPage/chosenDish.tsx
+++ src/app/screens/RestaurantPage/chosenDish.tsx
@@ -1,7 +1,105 @@
 import React from "react";
-// import { Container } from "@mui/material";
-// import { Route, Switch, useRouteMatch } from "react-router-dom";
+import { Box, Container, Stack } from "@mui/material";
+import { Swiper, SwiperSlide } from "swiper/react";
+import RemoveRedEyeIcon from "@mui/icons-material/RemoveRedEye";
+import Marginer from "../../components/marginer";
+import Button from "@mui/material/Button";
+import Rating from "@mui/material/Rating";
+import "swiper/css";
+import "swiper/css/free-mode";
+import "swiper/css/navigation";
+import "swiper/css/thumbs";
+import { FreeMode, Navigation, Thumbs } from "swiper";
+import { Favorite, FavoriteBorder } from "@mui/icons-material";
+import Checkbox from "@mui/material/Checkbox";
+
+const chosen_list = Array.from(Array(3).keys());
 
 export function ChosenDish() {
-  return <div className="chosen_dish_page">ChosenDish</div>;
+  const label = { inputProps: { "aria-label": "Checkbox demo" } };
+
+  return (
+    <div className="chosen_dish_page">
+      <Container className="dish_container">
+        {/* 1/2 Stack: Dish Slider */}
+        <Stack className="chosen_dish_slider">
+          {/* 1.1 Swiper: big pictures */}
+          <Swiper
+            className="dish_swiper"
+            loop={true}
+            spaceBetween={10}
+            navigation={true}
+            // thumbs={{ swiper: thumbsSwiper}}
+            modules={[FreeMode, Navigation, Thumbs]}
+          >
+            {chosen_list.map((ele) => {
+              const image_path = `/others/fish.jpg`;
+              return (
+                <SwiperSlide>
+                  <img
+                    style={{ width: "100%", height: "100%" }}
+                    src={image_path}
+                  />
+                </SwiperSlide>
+              );
+            })}
+          </Swiper>
+
+          {/* 1.2 div: small pictures */}
+          <div className={"one_dish_pics"}>
+            <img className="one_dish_pic" src="/others/fish.jpg" />
+            <img className="one_dish_pic" src="/others/fish.jpg" />
+            <img className="one_dish_pic" src="/others/fish.jpg" />
+          </div>
+        </Stack>
+
+        {/* 2/2 Stack: Dish Info */}
+        <Stack className="chosen_dish_info_container">
+          <Box className="chosen_dish_info_box">
+            <strong className={"dish_text"}>Delicious Fish</strong>
+            <span className={"resto_name"}>Texas De Brazil</span>
+            <Box className={"rating_box"}>
+              <Rating name="half-rating" defaultValue={3.5} precision={0.5} />
+              <div className={"evaluation_box"}>
+                <div
+                  style={{
+                    display: "flex",
+                    alignItems: "center",
+                    marginRight: "20px",
+                  }}
+                >
+                  <Checkbox
+                    {...label}
+                    icon={<FavoriteBorder />}
+                    checkedIcon={<Favorite style={{ color: "red" }} />}
+                    /*@ts-ignore*/
+                    checked={false}
+                  />
+                  <span>98</span>
+                </div>
+                <div style={{ display: "flex", alignItems: "center" }}>
+                  <RemoveRedEyeIcon sx={{ mr: "10px" }} />
+                  <span>1000</span>
+                </div>
+              </div>
+            </Box>
+            <p className={"dish_desc_info"}>Juda mazali baliq</p>
+            <Marginer
+              direction="horizontal"
+              height="2"
+              width="100%"
+              bg="#000000"
+            />
+            <div className={"dish_price_box"}>
+              <span>Narx:</span>
+              <span>$11</span>
+            </div>
+            <div className={"button_box"}>
+              <Button variant="contained">Savatga qo'shish</Button>
+            </div>
+          </Box>
+        </Stack>
+      </Container>
+    </div>
+  );
 }
