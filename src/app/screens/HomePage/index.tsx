f5ff1bb045dfc3c599debc00b73c70503519697b
Sirojiddin <siroj.origin.profile>
2023-12-22 22:06:28 +0900

    fix: usage of slice and selectors on homePage screen component

--- src/app/screens/HomePage/index.tsx
+++ src/app/screens/HomePage/index.tsx
@@ -9,11 +9,51 @@ import { Events } from "./events";
 import { Recommendations } from "./recommendations";
 import "../../../css/home.css";
 
+// REDUX
+import { useDispatch, useSelector } from "react-redux";
+import { Dispatch } from "@reduxjs/toolkit";
+import { createSelector } from "reselect";
+import { setTopRestaurants } from "./slice";
+import { retrieveTopRestaurants, retrieveBestRestaurants } from "./selector";
+import { Restaurant } from "../../../types/user";
+
+//** REDUX SLICE */
+const actionDispatch = (dispatch: Dispatch) => ({
+  setTopRestaurants: (data: Restaurant[]) => dispatch(setTopRestaurants(data)),
+});
+
+//** REDUX SELECTOR */
+const topRestaurantRetriever = createSelector(
+  retrieveTopRestaurants,
+  (topRestaurants) => ({
+    topRestaurants,
+  })
+);
+
+const bestRestaurantRetriever = createSelector(
+  retrieveBestRestaurants,
+  (bestRestaurants) => ({
+    bestRestaurants,
+  })
+);
+
 export function HomePage() {
+  /** INITIALIZATION */
+  const { setTopRestaurants } = actionDispatch(useDispatch());
+  const { topRestaurants } = useSelector(topRestaurantRetriever);
+  const { bestRestaurants } = useSelector(bestRestaurantRetriever);
+
+  console.log("topRestaurants>>>", topRestaurants);
+  console.log("bestRestaurants>>>", bestRestaurants);
+
   // selector: store => data
 
   useEffect(() => {
     // backend data request => data
+    const data: Restaurant[] = [];
+
+    setTopRestaurants(data);
+
     // slice: data => store
   }, []);
 
