5cdaa0fa0d6850b1818b8bd82188b8b95c71da8c
Sirojiddin <siroj.origin.profile>
2023-12-26 16:14:28 +0900

    feat: develop signup auth process

--- src/app/apiServices/memberApiService.ts
+++ src/app/apiServices/memberApiService.ts
@@ -30,6 +30,24 @@ class MemberApiService {
       throw err;
     }
   }
+
+  async signupRequest(signup_data: any) {
+    try {
+      const result = await axios.post(this.path + "/signup", signup_data, {
+        withCredentials: true,
+      });
+      console.log("state:", result.data.state);
+      assert.ok(result?.data, Definer.general_err1);
+      assert.ok(result?.data?.state != "fail", result?.data?.message);
+
+      const member: Member = result.data.data;
+      localStorage.setItem("member_data", JSON.stringify(member));
+      return member;
+    } catch (err: any) {
+      console.log(`ERROR ::: loginRequest ${err.message}`);
+      throw err;
+    }
+  }
 }
 
 export default MemberApiService;
